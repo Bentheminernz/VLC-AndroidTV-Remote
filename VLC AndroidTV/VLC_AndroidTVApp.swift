1d140839211851754494c6d9e3a8e43d85cbaa5f
Ben Lawrence <65065992+Bentheminernz@users.noreply.github.com>
2024-11-21 22:08:54 +1300

    Initial Commit

---
+++ VLC AndroidTV/VLC_AndroidTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLC_AndroidTVApp.swift
+//  VLC AndroidTV
+//
+//  Created by Ben Lawrence on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VLC_AndroidTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
